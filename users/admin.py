f0c05982b46b677bb50c9b74e0e70b58ff69e78b
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2025-04-01 16:12:47 +0200

    creating basic structure for the project? implementing models for books, users and borrowings. Implementing admin page with all models

---
+++ users/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("email", "first_name", "last_name", "is_staff", "is_superuser")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_staff", "is_superuser", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "first_name", "last_name", "password1", "password2"),
+        }),
+    )
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        return super().get_fieldsets(request, obj)
