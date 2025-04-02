e4a4dd9d7787b2023b1bbcf9e13d1c78103cb1d2
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2025-04-02 13:18:02 +0200

    implementing constraints and permissions to all models

--- books/views.py
+++ books/views.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
+from .permissions import IsAdminOrReadOnly
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework import serializers
 from .models import Borrowing
 
@@ -5,14 +7,34 @@ from .models import Borrowing
 class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
-        fields = "__all__"
-        read_only_fields = ("borrow_date", "actual_return_date")
+        fields = ("id", "book", "user", "borrow_date", "expected_return_date", "actual_return_date")
+        read_only_fields = ("id", "actual_return_date")
+
+    def validate_borrow_date(self, value):
+        from django.utils.timezone import now
+        if value < now().date():
+            raise serializers.ValidationError("Borrow date cannot be in the past.")
+        return value
+
+    def validate_expected_return_date(self, value):
+        borrow_date = self.initial_data.get("borrow_date")
+        if isinstance(borrow_date, str):
+            borrow_date = datetime.strptime(borrow_date, "%Y-%m-%d").date()
+
+        if value < borrow_date:
+            raise serializers.ValidationError("Expected return date must be the same or after borrow date.")
+        return value
 
     def create(self, validated_data):
-        borrowing = Borrowing.objects.create(**validated_data)
-        borrowing.book.inventory -= 1
-        borrowing.book.save()
-        return borrowing
+        book = validated_data["book"]
+
+        if book.inventory <= 0:
+            raise serializers.ValidationError("This book is not available for borrowing.")
+
+        book.inventory -= 1
+        book.save()
+
+        return super().create(validated_data)
 
 
 class BorrowingReturnSerializer(serializers.ModelSerializer):
@@ -20,14 +42,33 @@ class BorrowingReturnSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Borrowing
-        fields = ['id', 'book', 'user', 'borrow_date', 'expected_return_date', 'actual_return_date']
-        read_only_fields = ['id', 'book', 'user', 'borrow_date', 'expected_return_date']
+        fields = ["id", "book", "user", "borrow_date", "expected_return_date", "actual_return_date"]
+        read_only_fields = ["id", "book", "user", "borrow_date", "expected_return_date"]
+
+    def validate_actual_return_date(self, value):
+        from django.utils.timezone import now
+        if value < now().date():
+            raise serializers.ValidationError("Return date cannot be in the past.")
+        return value
 
     def update(self, instance, validated_data):
-        instance.actual_return_date = validated_data.get('actual_return_date', instance.actual_return_date)
-        instance.save()
+        if instance.actual_return_date is not None:
+            raise serializers.ValidationError("This borrowing has already been returned.")
 
+        actual_return_date = validated_data["actual_return_date"]
+
+        # Проверяем, что дата возврата не раньше даты займа
+        if actual_return_date < instance.borrow_date:
+            raise serializers.ValidationError("Return date cannot be earlier than borrow date.")
+
+        instance.actual_return_date = actual_return_date
+
+        # Увеличиваем инвентарь книги только если книга еще не была возвращена
         instance.book.inventory += 1
         instance.book.save()
 
+        instance.save()
+
         return instance
+
+
